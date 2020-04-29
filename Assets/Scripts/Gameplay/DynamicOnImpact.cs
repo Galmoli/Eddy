ffe6e8d878014f7af135605bf9a0b423ad0bd15e
Esmouc <miquelmartipou@gmail.com>
2020-04-30 01:00:56 +0200

    Greybox Iteration #7
    - Final greybox
    - Maybe add one more room?
    - Polish camera

---
+++ Assets/Scripts/Gameplay/DynamicOnImpact.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicOnImpact : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Player")
+        {
+
+            GetComponent<Rigidbody>().isKinematic = false;
+            
+        }
+    }
+}
