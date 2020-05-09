bd2234758dd2a584310e1a2a89ceb3a76927fd7c
Toni Galmés Oliver <tonigalmes98@gmail.com>
2020-05-09 17:24:04 +0200

    Dead state & Spawn

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerMovementController _movementController;
+    private PlayerCombatController _combatController;
+
+    private void Awake()
+    {
+        _movementController = GetComponent<PlayerMovementController>();
+        _combatController = GetComponent<PlayerCombatController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            SetDeadState();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Spawn();
+        }
+    }
+
+    private void SetDeadState()
+    {
+        _movementController.SetState(new DeadState());
+        _combatController.SetState(new DeadState());
+    }
+
+    private void Spawn()
+    {
+        _movementController.Spawn();
+        _movementController.SetState(new MoveState(_movementController));
+        _combatController.SetState(new IdleState(_combatController));
+    }
+}
---
+++ Assets/Scripts/Player/PlayerStates/DeadState.cs
@@ -0,0 +1,12 @@
+﻿public class DeadState : State
+{
+    public override void Enter()
+    {
+        //Set Dead Animation
+    }
+
+    public override void Interact()
+    {
+        //Move player to last checkpoint
+    }
+}
