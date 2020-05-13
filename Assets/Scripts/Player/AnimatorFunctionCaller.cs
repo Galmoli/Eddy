aa085587be2c948b3fa8cd1e9617a7ceeb5c2d61
DaniRiwez <danielrodriguezgarcia1999@gmail.com>
2020-05-13 15:51:00 +0200

    Scanner Activation Anim + Nail Sword Improved

--- Assets/Scripts/Player/AnimatorFunctionCaller.cs
+++ Assets/Scripts/Player/AnimatorFunctionCaller.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class AnimatorFunctionCaller : MonoBehaviour
 {
     public PlayerCombatController combatController;
+    public PlayerSwordScanner swordScanner;
 
     public void EnableTrigger()
     {
         combatController.swordTrigger.EnableTrigger();
+    }
+
+    public void FinishStab()
+    {
+        swordScanner.FinishStab();
     }
 }
