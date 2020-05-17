1a230f6d6977951c44fb4ee23a318e6036b4c387
Toni Galmés Oliver <tonigalmes98@gmail.com>
2020-05-18 01:35:15 +0200

    In-game Dialogues

---
+++ Assets/Scripts/UI/DialoguePopUpLine.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Line", menuName = "Dialogue PopUp Line")]
+public class DialoguePopUpLine : ScriptableObject
+{
+    [TextArea] public string line;
+}
