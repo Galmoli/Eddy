83c76fb1b4c74f90fe59987605503cd1b8a53de3
albertmc36 <albertmecu@gmail.com>
2020-05-18 03:22:47 +0200

    Enemy 2 FSMs

---
+++ Assets/Scripts/Enemies/Throw Hands Enemy/ThrowHandsEnemyDeathFSM.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ThrowHandsEnemyHitFSM))]
+
+public class ThrowHandsEnemyDeathFSM : MonoBehaviour
+{
+    public enum States
+    {
+        INITIAL,
+        HIT,
+        DEATH
+    }
+
+    private States currentState;
+
+    private ThrowHandsEnemyBlackboard blackboard;
+    private ThrowHandsEnemyHitFSM enemyHitFSM;
+
+    private void Start()
+    {
+        blackboard = GetComponent<ThrowHandsEnemyBlackboard>();
+        enemyHitFSM = GetComponent<ThrowHandsEnemyHitFSM>();
+    }
+
+    private void OnEnable()
+    {
+        currentState = States.INITIAL;
+    }
+
+    private void OnDisable()
+    {
+        enemyHitFSM.enabled = false;
+    }
+
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case States.INITIAL:
+                ChangeState(States.HIT);
+                break;
+            case States.HIT:
+                if(blackboard.healthPoints <= 0)
+                {
+                    ChangeState(States.DEATH);
+                }
+                break;
+        }
+    }
+
+    private void ChangeState(States newState)
+    {
+        switch (currentState)
+        {
+            case States.INITIAL:
+                break;
+            case States.HIT:
+                enemyHitFSM.enabled = false;
+                break;
+            case States.DEATH:
+                break;
+        }
+
+        switch (newState)
+        {
+            case States.INITIAL:
+                break;
+            case States.HIT:
+                enemyHitFSM.enabled = true;
+                break;
+            case States.DEATH:
+                gameObject.SetActive(false);
+                break;
+        }
+
+        currentState = newState;
+        blackboard.statesText.text = currentState.ToString();
+    }
+}
---
+++ Assets/Scripts/Enemies/Throw Hands Enemy/ThrowHandsEnemyHitFSM.cs
@@ -0,0 +1,104 @@
+﻿using Steerings;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ThrowHandsEnemyStunFSM))]
+
+public class ThrowHandsEnemyHitFSM : MonoBehaviour
+{
+    public enum States
+    {
+        INITIAL,
+        STUNNED,
+        STAGGERED
+    }
+
+    private States currentState;
+
+    private ThrowHandsEnemyBlackboard blackboard;
+    private ThrowHandsEnemyStunFSM enemyStunFSM;
+    private Rigidbody rigidBody;
+    private KinematicState kinematicState;
+
+    float timer = 0;
+
+    private void Start()
+    {
+        blackboard = GetComponent<ThrowHandsEnemyBlackboard>();
+        enemyStunFSM = GetComponent<ThrowHandsEnemyStunFSM>();
+        rigidBody = GetComponent<Rigidbody>();
+        kinematicState = GetComponent<KinematicState>();
+    }
+
+    private void OnEnable()
+    {
+        currentState = States.INITIAL;
+    }
+
+    private void OnDisable()
+    {
+        enemyStunFSM.enabled = false;
+    }
+
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case States.INITIAL:
+                ChangeState(States.STUNNED);
+                break;
+            case States.STUNNED:
+                if (blackboard.hit)
+                {
+                    ChangeState(States.STAGGERED);
+                }
+                break;
+            case States.STAGGERED:
+
+                if(timer >= blackboard.staggeredTime)
+                {
+                    ChangeState(States.STUNNED);
+                    break;
+                }
+
+                timer += Time.deltaTime;
+                
+                break;
+        }
+    }
+
+    private void ChangeState(States newState)
+    {
+        switch (currentState)
+        {
+            case States.INITIAL:
+                break;
+            case States.STUNNED:
+                enemyStunFSM.enabled = false;
+                blackboard.hit = false;
+                break;
+            case States.STAGGERED:
+                kinematicState.position = transform.position;
+                kinematicState.orientation = transform.eulerAngles.y;
+                kinematicState.linearVelocity = (blackboard.player.transform.position - transform.position).normalized;
+                timer = 0;
+                break;
+        }
+
+        switch (newState)
+        {
+            case States.INITIAL:
+                break;
+            case States.STUNNED:
+                enemyStunFSM.enabled = true;
+                break;
+            case States.STAGGERED:
+                rigidBody.AddForce(blackboard.hitDirection.normalized * blackboard.staggerImpulse, ForceMode.Impulse);
+                break;
+        }
+
+        currentState = newState;
+        blackboard.statesText.text = currentState.ToString();
+    }
+}
