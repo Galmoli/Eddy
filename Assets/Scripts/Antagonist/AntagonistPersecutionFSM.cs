1f30d319d19b1adced52c8edfa46d43c5e4d5f65
Esmouc <miquelmartipou@gmail.com>
2020-05-27 21:58:54 +0200

    Final Room Iteration

---
+++ Assets/Scripts/Antagonist/AntagonistPersecutionFSM.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AntagonistPersecutionFSM : MonoBehaviour
+{
+    public States currentState;
+
+    private AntagonistBlackboard blackboard;
+    private NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        blackboard = GetComponent<AntagonistBlackboard>();
+
+        currentState = States.INITIAL;
+    }
+
+    public void Reset()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case States.INITIAL:
+                ChangeState(States.GUARDING);
+                break;
+            case States.GUARDING:
+
+                Vector3 guardingPosition;
+
+                guardingPosition = transform.position - blackboard.player.transform.position;
+                guardingPosition = Vector3.ProjectOnPlane(guardingPosition, blackboard.guardingCol.transform.forward);
+                guardingPosition = blackboard.player.transform.position + guardingPosition;
+
+
+                navMeshAgent.SetDestination(blackboard.player.transform.position);
+
+                break;
+            case States.FOLLOWING:
+                break;
+            case States.PERSECUTION:
+                break;
+            case States.STUNNED:
+                break;
+            case States.APPEARING_DOWN:
+                break;
+            case States.APPEARING_ROTATING:
+                break;
+        }
+    }
+
+    public void ChangeState(States newState)
+    {
+        switch (currentState)
+        {
+            case States.INITIAL:
+                break;
+            case States.GUARDING:
+                break;
+            case States.FOLLOWING:
+                break;
+            case States.PERSECUTION:
+                break;
+            case States.STUNNED:
+                break;
+            case States.APPEARING_DOWN:
+                break;
+            case States.APPEARING_ROTATING:
+                break;
+        }
+
+        switch (newState)
+        {
+            case States.INITIAL:
+                break;
+            case States.GUARDING:
+                navMeshAgent.speed = blackboard.guardingSpeed;
+                break;
+            case States.FOLLOWING:
+                break;
+            case States.PERSECUTION:
+                blackboard.attackCollider.enabled = true;
+                blackboard.firstObstacle.SetActive(false);
+                break;
+            case States.STUNNED:
+                break;
+            case States.APPEARING_DOWN:
+                break;
+            case States.APPEARING_ROTATING:
+                break;
+        }
+
+        currentState = newState;
+    }
+
+    public enum States
+    {
+        INITIAL, GUARDING, FOLLOWING, PERSECUTION, STUNNED, APPEARING_DOWN, APPEARING_ROTATING
+    }
+}
+
+
---
+++ Assets/Scripts/Gameplay/EddyPipe.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EddyPipe : MonoBehaviour
+{
+    public Transform[] eddysInstancePos;
+    public GameObject EddyRagdoll;
+
+    public int numberOfInstances;
+    public float secondsInterval;
+
+    bool activated;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && !activated)
+        {
+            StartCoroutine(InstantiateEddies());
+            activated = true;
+        }
+    }
+
+    IEnumerator InstantiateEddies()
+    {
+        for (int i = 0; i < numberOfInstances; i++) {
+            int randomPos = Random.Range(0, eddysInstancePos.Length);
+            Instantiate(EddyRagdoll, eddysInstancePos[randomPos].position, eddysInstancePos[randomPos].rotation);
+            yield return new WaitForSeconds(secondsInterval);
+        }
+    }
+}
