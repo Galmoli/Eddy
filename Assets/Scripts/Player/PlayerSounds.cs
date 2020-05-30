a27adad11dae48973b873b5147cc552fbad53dd1
albertmc36 <albertmecu@gmail.com>
2020-05-30 15:05:18 +0200

    Player Sounds class added

---
+++ Assets/Scripts/Player/PlayerSounds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour
+{
+    [Header("Basics")]
+    public string stoneStepSoundPath;
+    public string woodStepSoundPath;
+    public string damageReceivedSoundPath;
+    public string deathSoundPath;
+    public string jumpSoundPath;
+    public string landSoundPath;
+
+    [Header("Combat")]
+    public string attackSoundPath;
+    public string comboAttackSoundPath;
+    public string areaAttackSoundPath;
+    public string areaAttackChargingSoundPath;
+    public string areaAttackChargedSoundPath;
+    public string enemyHitSoundPath;
+    public string enemyArmoredHitSoundPath;
+    public string woodObjectHitSoundPath;
+    public string metalObjectHitSoundPath;
+
+    [Header("Others")]
+    public string draggableObjectSoundPath;
+    public string elevatorSoundPath;
+}
