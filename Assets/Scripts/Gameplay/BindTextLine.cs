65489d3e7a1846fa87b2e1dd3b8233eec8129f41
Esmouc <miquelmartipou@gmail.com>
2020-06-04 17:21:51 +0200

    Added Fade to Black and Dialogue Scenes

---
+++ Assets/Scripts/Gameplay/BindTextLine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BindTextLine : MonoBehaviour
+{
+    TextMeshProUGUI text;
+    public DialoguePopUpLine line;
+
+    private void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        text.text = line.line;
+    }
+}
---
+++ Assets/Scripts/Gameplay/TransitionToBootScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionToBootScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   public void LoadGame()
+    {
+        SceneManager.LoadScene("Additive_BootScene");
+    }
+}
