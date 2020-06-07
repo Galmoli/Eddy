fa205dc346d88a896112d464e0d75f3e86924ace
DaniRiwez <danielrodriguezgarcia1999@gmail.com>
2020-06-08 01:46:00 +0200

    Eddy Attack Trail Particles

---
+++ Assets/Scripts/Player/Combat/PlayerCombatFX.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombatFX : MonoBehaviour
+{
+    public ParticleSystem swordTrailParticles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TrailOn()
+    {
+        swordTrailParticles.gameObject.SetActive(true);
+    }
+    public void TrailOff()
+    {
+        swordTrailParticles.gameObject.SetActive(false);
+    }
+}
