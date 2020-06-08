31e57bac39c5dca8e8f250600d5a91126fd0f6f3
Esmouc <miquelmartipou@gmail.com>
2020-06-08 17:14:11 +0200

    Added Helper Dialogues

---
+++ Assets/Scripts/Gameplay/Dialogue/DialogueHelperTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHelperTrigger : MonoBehaviour
+{
+    public int id;
+    public float waitToDisable;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            HelperDialogueController.Instance.ShowHelper(id);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(WaitToDisable());
+        }
+    }
+
+    IEnumerator WaitToDisable()
+    {
+        yield return new WaitForSeconds(waitToDisable);
+        HelperDialogueController.Instance.HideHelper(id);
+    }
+}
