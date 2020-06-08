da95fce19ea24c9ddc8914063e5543219c2dd50d
albertmc36 <albertmecu@gmail.com>
2020-06-08 12:58:53 +0200

    Camera movement improvement

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [Header("Basics")]
     public CameraRail rail;
     public Transform target;
-    public float movementSpeed = 1f;
-    public float rotationSpeed = 1f;
 
-    [Header("Look At Player / Nodes Rotation")]
+    [Header("Movement")]
+    public float maxMovementSpeed;
+    public float slowingRadius;
+
+    [Header("Rotation")]
+    public float rotationSpeed;
     public bool lookAtPlayer;
+
+    private Rigidbody rb;   
+    private Vector3 desiredVelocity = Vector3.zero;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {  
         if(rail != null)
         {
-            Vector3 pos = rail.ProjectPosition(target.position);
-
-            transform.position = Vector3.Lerp(transform.position, pos, movementSpeed * Time.deltaTime);
-
             if (lookAtPlayer)
             {
                 transform.LookAt(target.position);
@@ -33,7 +41,33 @@ public class CameraController : MonoBehaviour
         }
     }
 
-    public void SetPlayerPosition()
+    private void FixedUpdate()
+    {
+        if (rail != null)
+        {
+            Vector3 pos = rail.ProjectPosition(target.position);
+
+            //transform.position = Vector3.Lerp(transform.position, pos, movementSpeed * Time.deltaTime);
+
+            float distanceFromPosition = (pos - transform.position).magnitude;
+            float stoppingFactor;
+
+            if (slowingRadius > 0)
+            {
+                stoppingFactor = Mathf.Clamp(distanceFromPosition / slowingRadius, 0.0f, 1.0f);
+            }
+            else
+            {
+                stoppingFactor = Mathf.Clamp(distanceFromPosition, 0.0f, 1.0f);
+            }
+
+            desiredVelocity = (pos - transform.position).normalized * maxMovementSpeed * stoppingFactor;
+
+            rb.velocity += desiredVelocity - rb.velocity;
+        }
+    }
+
+    public void SetPositionImmediately()
     {
         transform.position = rail.ProjectPosition(target.position, true);
         transform.rotation = Quaternion.Euler(rail.ProjectRotation(target.position, transform.position));
