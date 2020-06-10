93c81101ccc76821aa3226ea79a954468f0d5f91
DaniRiwez <danielrodriguezgarcia1999@gmail.com>
2020-06-10 14:00:02 +0200

    Enemy Shadows

---
+++ Assets/Scripts/Gameplay/ShadowEnemies.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class ShadowEnemies : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 iniTarget;
+    public float timer;
+    public float lerpSpeed = 0.1f;
+    private bool hasBeenActivated;
+
+    void Start()
+    {
+        iniTarget = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !hasBeenActivated) StartCoroutine("ActivateShadows");
+    }
+
+    IEnumerator ActivateShadows()
+    {
+        while (Vector3.Distance(transform.position, target.position) > 0.1f)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, lerpSpeed);
+            yield return new WaitForEndOfFrame();
+        }
+        yield return new WaitForSeconds(timer);
+        while (Vector3.Distance(transform.position, iniTarget) > 0.1f)
+        {
+            transform.position = Vector3.Lerp(transform.position, iniTarget, lerpSpeed);
+            yield return new WaitForEndOfFrame();
+        }
+        Destroy(gameObject);
+    }
+}
