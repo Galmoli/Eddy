4c3aa7e3b57b935bf6b400430c7b8a808ecc0994
albertmc36 <albertmecu@gmail.com>
2020-06-11 03:49:28 +0200

    Prefab updated + adaptative speed improvement

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -12,17 +12,18 @@ public class CameraController : MonoBehaviour
     [Header("Movement")]
     public float maxMovementSpeed = 15f;
     public float maxDistance = 5f;
-    public float movementSpeedIncrement = 5f;
+    public float movementSpeedIncrement = 8f;
 
     private float currentMovementSpeed = 0f;
 
     [Header("Rotation")]
     public bool lookAtPlayer = false;
-    public float maxRotationSpeed = 15f;
+    public float cameraRotationSpeed = 2f;
+
+    /*public float maxRotationSpeed = 15f;
     public float maxAngle = 5f;
     public float rotationSpeedIncrement = 5f;
-
-    private float currentRotationSpeed = 0f;
+    private float currentRotationSpeed = 0f;*/
 
     void Update()
     {
@@ -48,15 +49,18 @@ public class CameraController : MonoBehaviour
             {
                 Vector3 rot = rail.ProjectRotation(target.position, transform.position);
 
-                float desiredRotationSpeed = Mathf.Max(Mathf.Abs(transform.eulerAngles.x - rot.x), Mathf.Abs(transform.eulerAngles.y - rot.y), Mathf.Abs(transform.eulerAngles.z - rot.z)) * maxRotationSpeed / maxAngle;
+                /*float desiredRotationSpeed = Mathf.Max(Mathf.Abs(rot.x - transform.eulerAngles.x), Mathf.Abs(rot.y - transform.eulerAngles.y), Mathf.Abs(rot.z - transform.eulerAngles.z)) * maxRotationSpeed / maxAngle;
 
-                if (currentRotationSpeed < desiredRotationSpeed) currentRotationSpeed += rotationSpeedIncrement * Time.deltaTime;
+                if (currentRotationSpeed < desiredRotationSpeed) currentRotationSpeed += movementSpeedIncrement * Time.deltaTime;
                 else if (currentRotationSpeed > desiredRotationSpeed) currentRotationSpeed = desiredRotationSpeed;
+
+                Debug.Log(desiredRotationSpeed);
 
                 if (currentRotationSpeed > maxRotationSpeed) currentRotationSpeed = maxRotationSpeed;
                 else if (currentRotationSpeed < 0) currentRotationSpeed = 0f;
 
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(rot), currentRotationSpeed * Time.deltaTime);  
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(rot), currentRotationSpeed * Time.deltaTime);*/
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(rot), 2 * Time.deltaTime);
             }
         }
     }
