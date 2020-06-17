9299c6ec574e30f774980555738b363c1747c3e3
Esmouc <miquelmartipou@gmail.com>
2020-06-17 18:16:36 +0200

    Added music to menu & transition to cinematic

---
+++ Assets/Scripts/Gameplay/Menus/MainMenuMusic.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class MainMenuMusic : MonoBehaviour
+{
+    [Header("Music References")]
+    [FMODUnity.EventRef] public string mainMenuMusic;
+
+    EventInstance mainMenu_Event;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayMusic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        StopMusic();
+    }
+
+    public void PlayMusic()
+    {
+        mainMenu_Event = RuntimeManager.CreateInstance(mainMenuMusic);
+
+        if (!mainMenu_Event.Equals(null))
+        {
+            mainMenu_Event.start();
+        }
+    }
+
+    public void StopMusic()
+    {
+        mainMenu_Event.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+}
