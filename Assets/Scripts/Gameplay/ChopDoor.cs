9c5fa3d27c146cb0812e13f4f31a5e57d4b253d6
Esmouc <miquelmartipou@gmail.com>
2020-06-18 15:06:48 +0200

    Update ChopDoor.cs

--- Assets/Scripts/Gameplay/ChopDoor.cs
+++ Assets/Scripts/Gameplay/ChopDoor.cs
@@ -19,7 +19,7 @@ public class ChopDoor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!end)
+        if (!end && Time.timeScale != 0)
             rb.AddForce(force * Vector3.down, ForceMode.Force);
     }
 
